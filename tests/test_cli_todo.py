3da0ed630c7249dd5f01e221eaa993f06b2a1668
Михаил Артемьев <m.artemiev@fund-f.ru>
2022-07-18 21:20:51 +0300

    first test fixed

--- tests/test_cli_todo.py
+++ tests/test_cli_todo.py
@@ -1,6 +1,9 @@
 from typer.testing import CliRunner
+import sqlite3
+import os
 from cli_todo import __version__
 from cli_todo.cli import cli
+from cli_todo.cli import Db
 
 runner = CliRunner()
 
@@ -9,14 +12,20 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-def test_cli():
+def test_cli(monkeypatch):
+    def get_db(self):
+        return sqlite3.connect('/tmp/test.db')
+
+    monkeypatch.setattr(Db, 'get_db', get_db)
+
     result = runner.invoke(cli, ['add', '--text', 'test todo'])
     assert result.exit_code == 0
     result = runner.invoke(cli, ['list'])
     assert result.exit_code == 0
     assert 'test todo' in result.stdout
     index = int(result.stdout.split(',')[0])
-    result = runner.invoke(cli, ['remove', index])
+    result = runner.invoke(cli, ['remove', str(index)])
     assert result.exit_code == 0
     result = runner.invoke(cli, ['list'])
     assert 'test todo' not in result.stdout
+    os.remove('/tmp/test.db')
